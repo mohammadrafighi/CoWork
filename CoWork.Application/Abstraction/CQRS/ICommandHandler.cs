092807bebaa4c046bbd8a8538c608b203e7ed067
amirmahdinadem <amirmahdinadem@gmail.com>
2026-02-21 12:01:39 +0330

    AddIdentity And Cqrs Abstraction

---
+++ CoWork.Application/Abstraction/CQRS/ICommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoWork.Application.Abstraction.CQRS
+{
+    public interface ICommandHandler<TCommand> : IRequestHandler<TCommand>
+        where TCommand : ICommand
+    {
+
+    }
+    public interface ICommandHandler<TCommand,TResult> : IRequestHandler<TCommand,TResult>
+    where TCommand : ICommand<TResult>
+    {
+
+    }
+}
